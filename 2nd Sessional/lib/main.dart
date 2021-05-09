bebf10556c1625aa23c4b8a18248b0cf3b154485
saeed123991 <79787125+saeed123991@users.noreply.github.com>
2021-05-09 09:38:38 -0700

    commit3

--- 2nd Sessional/lib/main.dart
+++ 2nd Sessional/lib/main.dart
@@ -1,3 +1,4 @@
+
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -48,4 +49,81 @@ class _SplashScreenState extends State<SplashScreen> {
       home: sp,
     );
   }
-}
+}drawer: Drawer(
+            child: ListView(
+              children: <Widget>[
+
+
+                Container(
+                  padding: EdgeInsets.only(top: 30.0, bottom: 30.0, left: 60.0),
+                  color: Colors.lightGreen,
+
+                  child: Center(
+
+                    child: ListTile(
+                      title: Text(
+                        'Home',
+
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      onTap: () {
+                        //correct = 0;
+                        // index = 0;
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Home()),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 30.0, bottom: 30.0, left: 30.0),
+                  color: Colors.lightGreen,
+                  child: ListTile(
+                    title: Text(
+                      'Contact Us',
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => contactus()
+                           ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              FlatButton(
+                shape: RoundedRectangleBorder(
+
+                  borderRadius: BorderRadius.circular(60.0),
+                  side: BorderSide(
+                    color: Colors.blue,
+                  ),
+                ),
+                onPressed: () {
+                  setState(() {
+                    Navigator.push(
+                      context,
+                      new MaterialPageRoute(builder: (context) => beginner()
+
+                      ),
+                    );
+                  });
+                },
