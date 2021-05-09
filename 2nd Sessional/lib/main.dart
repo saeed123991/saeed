91feccdd42d8122bd134f198b01a6b583a8ea151
saeed123991 <79787125+saeed123991@users.noreply.github.com>
2021-05-09 09:40:42 -0700

    commit5

--- 2nd Sessional/lib/main.dart
+++ 2nd Sessional/lib/main.dart
@@ -207,3 +207,107 @@ class contactus extends StatelessWidget {
             ),
           ),
         ),
+  drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+
+              Container(
+                padding: EdgeInsets.only(top: 30.0, bottom: 30.0, left: 60.0),
+                color: Colors.lightGreen,
+                child: Center(
+                  child: ListTile(
+                    title: Text(
+                      'Home',
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    onTap: () {
+                   //   correct = 0;
+                   //   index = 0;
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Home()),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 30.0, bottom: 30.0, left: 30.0),
+                color: Colors.lightGreen,
+                child: ListTile(
+                  title: Text(
+                    'Contact Us',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => contactus()),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Contact Us',
+                style: TextStyle(
+                  fontFamily: 'Quando',
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40.0,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              CircleAvatar(
+                backgroundImage: AssetImage('images/af.jpg'),
+                backgroundColor: Colors.yellow,
+                radius: 70.0,
+              ),
+              Text(
+                'Taimoor Khan',
+                style: TextStyle(
+                  fontFamily: 'Satisfy',
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.0,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 350.0,
+                child: Divider(
+                  color: Colors.white,
+                  thickness: 4.0,
+                ),
+              ),
+              Text(
+                'BS-CS Student/ Developer',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  letterSpacing: 5.0,
+                ),
+              ),
+              SizedBox(
+                height: 60.0,
+                width: 350.0,
+                child: Divider(
+                  color: Colors.white,
+                  thickness: 4.0,
+                ),
+              ),
